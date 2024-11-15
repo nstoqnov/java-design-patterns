332c7098292807e97d230e5c6b8c01a66a8e6ec9
nikolaystoyanov <stoqnovn@yahoo.com>
2024-11-15 21:01:15 +0200

    factory method - buttons

---
+++ Creational/factory-method/src/main/java/org/example/buttons/HtmlButton.java
@@ -0,0 +1,13 @@
+package org.example.buttons;
+
+public class HtmlButton implements Button {
+    @Override
+    public void render() {
+        System.out.println("<button>Test Button</button>");
+    }
+
+    @Override
+    public void onClick() {
+        System.out.println("Click Button says - Hello World!");
+    }
+}
