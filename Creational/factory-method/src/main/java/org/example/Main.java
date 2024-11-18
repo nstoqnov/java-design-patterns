af23f4c3eba80e2a76a9774ee9aa9ee2dc9b24a1
nikolaystoyanov <stoqnovn@yahoo.com>
2024-11-18 21:08:06 +0200

    abstract factory  method

--- Creational/factory-method/src/main/java/org/example/Main.java
+++ Creational/factory-method/src/main/java/org/example/Main.java
@@ -7,13 +7,14 @@ import org.example.factory.WindowsDialog;
 public class Main {
     private static Dialog dialog;
     public static void main(String[] args) {
+
         configure();
         runBusinessLogic();
     }
 
     private static void configure() {
         System.out.println(System.getProperty("os.name"));
-        if(System.getProperty("os.name").equals("Mac OS X")){
+        if(System.getProperty("os.name").equals("Macs OS X")){
             dialog = new WindowsDialog();
         }else{
             dialog = new HtmlDialog();
