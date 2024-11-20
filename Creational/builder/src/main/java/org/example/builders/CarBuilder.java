c62b252e26dda5ba60f917da4ac90c5cffd37a74
nikolaystoyanov <stoqnovn@yahoo.com>
2024-11-20 21:18:01 +0200

    builder pattern - car builder

--- Creational/builder/src/main/java/org/example/builders/CarBuilder.java
+++ Creational/builder/src/main/java/org/example/builders/CarBuilder.java
@@ -1,4 +1,39 @@
 package org.example.builders;
 
-public class CarBuilder {
+public class CarBuilder implements Builder{
+    private CarType type;
+    private int seats;
+    private Engine engine;
+    private Transmission transmission;
+    private TripComputer tripComputer;
+    private GPSNavigator gpsNavigator;
+    @Override
+    public void setCarType(CarType carType) {
+        this.carType = carType;
+    }
+
+    @Override
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    @Override
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+
+    @Override
+    public void setTripComputer(TripComputer tripComputer) {
+        this.tripComputer = tripComputer;
+    }
+
+    @Override
+    public void setGPSNavigator(GPSNavigator gpsNavigator) {
+        this.gpsNavigator= gpsNavigator;
+    }
 }
