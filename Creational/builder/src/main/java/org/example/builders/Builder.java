f7ea7db2e674fd667be8bf47b2d9dc6067911b3b
nikolaystoyanov <stoqnovn@yahoo.com>
2024-11-20 21:17:34 +0200

    builder pattern - add builder

---
+++ Creational/builder/src/main/java/org/example/builders/Builder.java
@@ -0,0 +1,11 @@
+package org.example.builders;
+
+public interface Builder {
+    void setCarType(CarType carType);
+    void setSeats(int seats);
+    void setEngine(Engine engine);
+    void setTransmission(Transmission transmission);
+    void setTripComputer(TripComputer tripComputer);
+    void setGPSNavigator(GPSNavigator gpsNavigator);
+
+}
---
+++ Creational/builder/src/main/java/org/example/builders/CarBuilder.java
@@ -0,0 +1,4 @@
+package org.example.builders;
+
+public class CarBuilder {
+}
