c2ff72f657ec1828770fc681d5ba9dc377190c1e
nikolaystoyanov <stoqnovn@yahoo.com>
2024-11-21 19:52:48 +0200

    builder pattern - test

--- Creational/builder/src/main/java/org/example/Main.java
+++ Creational/builder/src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import org.example.builders.CarBuilder;
+import org.example.builders.CarManualBuilder;
+import org.example.cars.Car;
+import org.example.cars.Manual;
+import org.example.director.Director;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Director director = new Director();
+
+        CarBuilder builder = new CarBuilder();
+        director.constructSportsCar(builder);
+
+        Car car = builder.getResults();
+        System.out.println("Car built:\n" + car.getCarType());
+
+
+        CarManualBuilder manualBuilder = new CarManualBuilder();
+
+
+        director.constructSportsCar(manualBuilder);
+        Manual carManual = manualBuilder.getResult();
+        System.out.println("\nCar manual built:\n" + carManual.print());
     }
 }
