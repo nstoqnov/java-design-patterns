4dfae3e75ec416801d658788391a730331d72d28
nikolaystoyanov <stoqnovn@yahoo.com>
2024-11-22 22:46:13 +0200

    prototype design pattern - shape override equals method

--- Creational/prototype/src/main/java/org/example/shapes/Shape.java
+++ Creational/prototype/src/main/java/org/example/shapes/Shape.java
@@ -1,5 +1,7 @@
 package org.example.shapes;
 
+import java.util.Objects;
+
 public abstract class Shape {
     public int x;
     public int y;
@@ -16,5 +18,10 @@ public abstract class Shape {
         }
     }
     public abstract Shape clone();
-    
+    @Override
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Shape)) return false;
+        Shape shape2 = (Shape) object2;
+        return shape2.x == x && shape2.y == y && Objects.equals(shape2.color, color);
+    }
 }
