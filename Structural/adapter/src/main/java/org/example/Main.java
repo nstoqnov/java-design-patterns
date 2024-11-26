f60c7ccca6097593b5ef6c00c1a680a5a61180a4
nikolaystoyanov <stoqnovn@yahoo.com>
2024-11-26 20:42:20 +0200

    adapter design pattern - demo

--- Structural/adapter/src/main/java/org/example/Main.java
+++ Structural/adapter/src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import org.example.adapters.SquarePegAdapter;
+import org.example.round.RoundHole;
+import org.example.round.RoundPeg;
+import org.example.square.SquarePeg;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        RoundHole hole = new RoundHole(6);
+        RoundPeg rpeg = new RoundPeg(5);
+        if (hole.fits(rpeg)) {
+            System.out.printf("Round peg r%.2f fits round hole r%.2f%n", rpeg.getRadius(),hole.getRadius());
+        }
+
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(20);
+
+        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+        if (hole.fits(smallSqPegAdapter)) {
+            System.out.printf("Square peg w%.2f fits round hole r%.2f%n", smallSqPeg.getWidth(), hole.getRadius());
+        }
+        if (!hole.fits(largeSqPegAdapter)) {
+            System.out.printf("Square peg w%.2f does not fit into round hole r%.2f%n", largeSqPeg.getWidth(), hole.getRadius());
+        }
     }
 }
