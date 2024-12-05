21456bedf28469d304bcc2c20dbbaf5c5b3aa0aa
Nikolay Stoyanov <stoqnovn@yahoo.com>
2024-12-05 05:02:50 +0200

    composite design pattern - test

--- Structural/composite/src/main/java/org/example/Main.java
+++ Structural/composite/src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import org.example.editor.ImageEditor;
+import org.example.shapes.Circle;
+import org.example.shapes.CompoundShape;
+import org.example.shapes.Dot;
+
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ImageEditor editor = new ImageEditor();
+
+        editor.loadShapes(
+                new Circle(10, 10, 10, Color.BLUE),
+
+                new CompoundShape(
+                        new Circle(110, 110, 50, Color.RED),
+                        new Dot(160, 160, Color.RED)
+                ),
+
+                new CompoundShape(
+                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Dot(240, 240, Color.GREEN),
+                        new Dot(240, 360, Color.GREEN),
+                        new Dot(360, 360, Color.GREEN),
+                        new Dot(360, 240, Color.GREEN)
+                )
+        );
     }
 }
