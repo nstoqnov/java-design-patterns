903b00fa28156fcc85affb850cb581d3976a2165
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-01-02 21:47:24 +0200

    Day 1- questionary

--- JavaExercise/src/main/java/org/example/Main.java
+++ JavaExercise/src/main/java/org/example/Main.java
@@ -2,8 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //
-        //
-        System.out.println("Hello world!");
+        /*Day 1 questions:
+        Q:1. What is Java programming language and what are its key features?
+        A: Java is one of the most popular Object oriented programming languages in the world. One of its key features is that the java programs can be executed on different platforms and operating systems(WORA- Write once run anywhere).
+
+        2. Explain the process of setting up the Java development environment, including the installation of JDK and an IDE.
+        It's a simple process for which can be used official vendors of IDE, e.g. JetBrains.
+        3. What is the Java Virtual Machine (JVM) and what is its role in executing Java programs?
+        Java Virtual machine is an abstract machine. It is a runtime environment that executes bytecode files(.class)
+        Implementation of JVM depends on the hardware which wll be executed on. This gives the possibility to execute the same program on different everywhere.
+        JVM is part of JRE which is part of JDK.
+        4. Describe the steps involved in compiling and running a Java program using the command line.
+        
+        5. How does Java handle memory management and what is the significance of the garbage collector?
+        6. Compare and contrast Eclipse and IntelliJ IDEA as Java IDEs, highlighting their key features and differences.
+        7. What are the basic data types available in Java, and how are they used in variable declarations and assignments?
+        */
     }
 }
