e3e9a183a5e205370bf95d3d4f74d841d3e353a2
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-01-02 21:59:40 +0200

    Day 1- questionary

--- JavaExercise/src/main/java/org/example/Main.java
+++ JavaExercise/src/main/java/org/example/Main.java
@@ -13,10 +13,24 @@ public class Main {
         Implementation of JVM depends on the hardware which wll be executed on. This gives the possibility to execute the same program on different everywhere.
         JVM is part of JRE which is part of JDK.
         4. Describe the steps involved in compiling and running a Java program using the command line.
-        
+        When we compile .java files, .class files are generated. They are bytecode files.
+        Then JVM loading .class files using Class Loader.
+        Then JVM verifies all the code and execute it.
         5. How does Java handle memory management and what is the significance of the garbage collector?
+        Java automatically manages the memory with the help of the garbage collector.
+        JVM defines different memory areas:
+        -Heap Area
+        -Method Area
+        -JVM Stacks
+        -Native Method Stacks
+        -Program Counter (PC) Registers
+        Garbage collector runs in background and clear the memory from objects that are no longer used.
         6. Compare and contrast Eclipse and IntelliJ IDEA as Java IDEs, highlighting their key features and differences.
         7. What are the basic data types available in Java, and how are they used in variable declarations and assignments?
+        Basic data types are also called data primitives:
+        int, short, long, float, double, boolean, char
+        They can be used in declaration of variable as follows:
+        int number = 1;
         */
     }
 }
