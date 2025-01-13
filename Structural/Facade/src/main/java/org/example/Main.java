465fecff57c3ade0d850fbcc552f5ef0d4b54b64
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-01-13 20:12:34 +0200

    facade design pattern - demo

--- Structural/Facade/src/main/java/org/example/Main.java
+++ Structural/Facade/src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.facade.VideoConversionFacade;
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
     }
 }
