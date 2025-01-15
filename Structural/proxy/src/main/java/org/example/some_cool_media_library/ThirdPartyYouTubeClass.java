ee3435ab47f68b45af6872eb9e6a223f962aa9c4
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-01-15 20:09:02 +0200

    proxy design pattern - third party lib

---
+++ Structural/proxy/src/main/java/org/example/some_cool_media_library/ThirdPartyYouTubeClass.java
@@ -0,0 +1,13 @@
+package org.example.some_cool_media_library;
+
+public class ThirdPartyYouTubeClass implements ThirdPartyYouTubeLib{
+    @Override
+    public HashMap<String, Video> popularVideos() {
+        return null;
+    }
+
+    @Override
+    public Video getVideo(String videoId) {
+        return null;
+    }
+}
