f02caadc1e64191d9b748c53c138c926c0e89e57
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-01-24 21:05:46 +0200

    command design pattern - demo

--- Behavioral/Command/src/main/java/org/example/Main.java
+++ Behavioral/Command/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.editor.Editor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Editor editor = new Editor();
+        editor.init();
     }
 }
