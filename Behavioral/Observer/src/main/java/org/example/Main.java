2bb0100fa6f05e9cd9c0f4e93176b6bc7e531628
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-03-07 17:25:14 +0200

    observer design pattern - demo

--- Behavioral/Observer/src/main/java/org/example/Main.java
+++ Behavioral/Observer/src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.editor.Editor;
+import org.example.listeners.EmailNotificationListener;
+import org.example.listeners.LogOpenListener;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Editor editor = new Editor();
+        editor.events.subscribe("open", new LogOpenListener("/file.txt"));
+        editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
+
+        try {
+            editor.openFile("test.txt");
+            editor.saveFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
