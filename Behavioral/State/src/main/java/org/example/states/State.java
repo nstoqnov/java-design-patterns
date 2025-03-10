87edf2d8435a4d9414579454a084d2c2da509cd2
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-03-10 19:26:48 +0200

    state design pattern - state abstract class

--- Behavioral/State/src/main/java/org/example/states/State.java
+++ Behavioral/State/src/main/java/org/example/states/State.java
@@ -1,4 +1,14 @@
 package org.example.states;
 
 public abstract class State {
+    Player player;
+
+    State(Player player) {
+        this.player = player;
+    }
+
+    public abstract String onLock();
+    public abstract String onPlay();
+    public abstract String onNext();
+    public abstract String onPrevious();
 }
