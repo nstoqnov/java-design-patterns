8e19cfcfa1bd63b78052b1e307bfb39019c241ab
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-03-12 19:29:59 +0200

    state design pattern - ready state

--- Behavioral/State/src/main/java/org/example/states/ReadyState.java
+++ Behavioral/State/src/main/java/org/example/states/ReadyState.java
@@ -1,4 +1,30 @@
 package org.example.states;
 
-public class ReadyState {
+public class ReadyState extends State {
+    public ReadyState(Player player) {
+        super(player);
+    }
+
+    @Override
+    public String onLock() {
+        player.changeState(new LockedState(player));
+        return "Locked...";
+    }
+
+    @Override
+    public String onPlay() {
+        String action = player.startPlayback();
+        player.changeState(new PlayingState(player));
+        return action;
+    }
+
+    @Override
+    public String onNext() {
+        return "Locked...";
+    }
+
+    @Override
+    public String onPrevious() {
+        return "Locked...";
+    }
 }
