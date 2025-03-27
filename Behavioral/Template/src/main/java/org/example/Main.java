5d08c325cd3d9727184ff18f994138b9de3dc7dd
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-03-27 16:58:15 +0200

    template design pattern - demo

--- Behavioral/Template/src/main/java/org/example/Main.java
+++ Behavioral/Template/src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.example.networks.Facebook;
 import org.example.networks.Network;
+import org.example.networks.Twitter;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,6 +17,18 @@ public class Main {
         System.out.print("Input password: ");
         String password = reader.readLine();
 
+        System.out.print("Input message: ");
+        String message = reader.readLine();
 
+        System.out.println("\nChoose social network for posting message.\n" +
+                "1 - Facebook\n" +
+                "2 - Twitter");
+        int choice = Integer.parseInt(reader.readLine());
+        if (choice == 1) {
+            network = new Facebook(userName, password);
+        } else if (choice == 2) {
+            network = new Twitter(userName, password);
+        }
+        network.post(message);
     }
 }
