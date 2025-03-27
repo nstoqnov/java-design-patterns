96169c6ba15327642466d5cd699f274ae9a4d7b4
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-03-27 16:57:04 +0200

    template design pattern - read name and password

--- Behavioral/Template/src/main/java/org/example/Main.java
+++ Behavioral/Template/src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.example.networks.Network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Network network = null;
+        System.out.print("Input user name: ");
+        String userName = reader.readLine();
+        System.out.print("Input password: ");
+        String password = reader.readLine();
+
+
     }
 }
