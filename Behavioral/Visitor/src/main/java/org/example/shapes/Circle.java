d865c631ed580177cc0fc568e1d209663f0f56dd
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-03-28 17:03:43 +0200

    visitor design pattern - circle shape class

--- Behavioral/Visitor/src/main/java/org/example/shapes/Circle.java
+++ Behavioral/Visitor/src/main/java/org/example/shapes/Circle.java
@@ -1,4 +1,17 @@
 package org.example.shapes;
 
-public class Circle {
+public class Circle extends Dot{
+    private int radius;
+
+    public Circle(int id, int x, int y, int radius) {
+        super(id, x, y);
+        this.radius = radius;
+    }
+    @Override
+    public String accept(Visitor visitor) {
+        return null;
+    }
+    public int getRadius() {
+        return radius;
+    }
 }
