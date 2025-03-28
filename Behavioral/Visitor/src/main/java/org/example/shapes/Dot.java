191bff80f5fed809334be0a5a661331130a8e08a
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-03-28 17:01:56 +0200

    visitor design pattern - dot shape class

--- Behavioral/Visitor/src/main/java/org/example/shapes/Dot.java
+++ Behavioral/Visitor/src/main/java/org/example/shapes/Dot.java
@@ -1,4 +1,40 @@
 package org.example.shapes;
 
-public class Dot {
+public class Dot implements Shape{
+    private int id;
+    private int x;
+    private int y;
+
+    public Dot(){}
+
+    public Dot(int id, int x, int y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+    @Override
+    public void move(int x, int y) {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public String accept(Visitor visitor) {
+        return null;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
