ae9a6310d047e9242d4786eb85148f07cda99e86
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-04-01 20:33:22 +0300

    visitor design pattern - visitor interface

--- Behavioral/Visitor/src/main/java/org/example/visitor/Visitor.java
+++ Behavioral/Visitor/src/main/java/org/example/visitor/Visitor.java
@@ -1,4 +1,16 @@
 package org.example.visitor;
 
+import org.example.shapes.Circle;
+import org.example.shapes.CompoundShape;
+import org.example.shapes.Dot;
+import org.example.shapes.Rectangle;
+
 public interface Visitor {
+    String visitDot(Dot dot);
+
+    String visitCircle(Circle circle);
+
+    String visitRectangle(Rectangle rectangle);
+
+    String visitCompoundGraphic(CompoundShape cg);
 }
