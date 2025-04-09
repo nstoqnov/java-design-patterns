f82ddeffea81b0ac4d19a394a222a773fd37e797
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-04-09 19:31:47 +0300

    exerices

--- Behavioral/Exersice/src/main/java/org/example/Main.java
+++ Behavioral/Exersice/src/main/java/org/example/Main.java
@@ -3,58 +3,50 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+    public static ListNode resList = new ListNode(0);
+    public static ListNode head = resList;
+    public static int carry = 0;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[][] matrix = new int[9][9];
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                matrix[i][j] = scanner.nextInt();
-            }
+
+
+        ListNode l1 = new ListNode(2);
+        ListNode l1p = new ListNode(4);
+        ListNode l1pp = new ListNode(3);
+
+        l1.next = l1p;
+        l1p.next = l1pp;
+
+        ListNode l2 = new ListNode(5);
+        ListNode l2p = new ListNode(6);
+        ListNode l2pp = new ListNode(4);
+
+        l2.next = l2p;
+        l2p.next = l2pp;
+
+        addTwoNumbers(l1, l2);
+
+
+
+
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int sum = l1.val + l2.val + carry;
+        carry  = sum / 10;
+        resList.next = new ListNode(sum % 10);
+        resList = resList.next;
+
+        if(l1.next != null && l2.next != null){
+            addTwoNumbers(l1.next, l2.next);
+        } else if (l1.next != null) {
+            addTwoNumbers(l1.next, new ListNode(0));
+        } else if (l2.next != null) {
+            addTwoNumbers(new ListNode(0), l2.next);
+        }else if(carry > 0){
+            resList.next = new ListNode(1);
+            resList = resList.next;
         }
-        scanner.close();
-
-        for (int i = 0; i < 3; i++){
-
-            for (int j = 0; j < 3; j++){
-//                System.out.println(matrix[(i*3)][(j*3)] + " " + matrix[(i*3)][(j*3) + 1] + " " + matrix[(i*3)][(j*3) + 2] );
-//                System.out.println(matrix[(i*3) + 1][(j*3)] + " " + matrix[(i*3) + 1][(j*3) + 1] + " " + matrix[(i*3) + 1][(j*3) + 2] );
-//                System.out.println(matrix[(i*3) + 2][(j*3)] + " " + matrix[(i*3) + 2][(j*3) + 1] + " " + matrix[(i*3) + 2][(j*3) + 2] );
-                int sum = matrix[(i*3)][(j*3)] + matrix[(i*3)][(j*3) + 1] + matrix[(i*3)][(j*3) + 2] +
-                        matrix[(i*3) + 1][(j*3)] + matrix[(i*3) + 1][(j*3) + 1] + matrix[(i*3) + 1][(j*3) + 2] +
-                        matrix[(i*3) + 2][(j*3)] + matrix[(i*3) + 2][(j*3) + 1] + matrix[(i*3) + 2][(j*3) + 2];
-                int target = 45 - sum;
-                if(target == 0){
-
-                }else{
-                    if(matrix[(i*3)][(j*3)] == 0){
-                        matrix[(i*3)][(j*3)] = target;
-                    }else if(matrix[(i*3)][(j*3) + 1] == 0){
-                        matrix[(i*3)][(j*3) + 1] = target;
-                    } else if (matrix[(i*3)][(j*3) + 2] == 0) {
-                        matrix[(i*3)][(j*3) + 2] = target;
-                    } else if (matrix[(i*3) + 1][(j*3)] == 0) {
-                        matrix[(i*3) + 1][(j*3)] = target;
-                    } else if (matrix[(i*3) + 1][(j*3) + 1] == 0) {
-                        matrix[(i*3) + 1][(j*3) + 1] = target;
-                    } else if (matrix[(i*3) + 1][(j*3) + 2] == 0) {
-                        matrix[(i*3) + 1][(j*3) + 2] = target;
-                    } else if (matrix[(i*3) + 2][(j*3)] == 0) {
-                        matrix[(i*3) + 2][(j*3)] = target;
-                    } else if (matrix[(i*3) + 2][(j*3) + 1] == 0) {
-                        matrix[(i*3) + 2][(j*3) + 1] = target;
-                    } else if (matrix[(i*3) + 2][(j*3) + 2] == 0) {
-                        matrix[(i*3) + 2][(j*3) + 2] = target;
-                    }
-                }
-            }
-        }
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
+        return head.next;
     }
 }
