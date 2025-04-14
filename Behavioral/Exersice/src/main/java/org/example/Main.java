fd6e84437ddd32e0dc6a1e30c4af10300772b6ca
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-04-14 20:20:57 +0300

    sudoku solver

--- Behavioral/Exersice/src/main/java/org/example/Main.java
+++ Behavioral/Exersice/src/main/java/org/example/Main.java
@@ -3,45 +3,76 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    public static ListNode resList = new ListNode(0);
-    public static ListNode head = resList;
-    public static int carry = 0;
     public static void main(String[] args) {
 
-        ListNode l1 = new ListNode(2);
-        ListNode l1p = new ListNode(4);
-        ListNode l1pp = new ListNode(3);
+        int[][] matrix = {
+                {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                {6, 0, 2, 0, 9, 5, 3, 4, 8},
+                {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                {2, 0, 7, 0, 1, 9, 6, 3, 5},
+                {3, 4, 5, 2, 8, 6, 1, 7, 9}
+        };
 
-        l1.next = l1p;
-        l1p.next = l1pp;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if(matrix[i][j] == 0){
+                    for (int k = 1; k <= 9; k++) {
+                        if(isValidPlacement(matrix,k,i,j)){
+                            matrix[i][j] = k;
+                        }
+                    }
+                }
+            }
+        }
 
-        ListNode l2 = new ListNode(5);
-        ListNode l2p = new ListNode(6);
-        ListNode l2pp = new ListNode(4);
-
-        l2.next = l2p;
-        l2p.next = l2pp;
-
-        addTwoNumbers(l1, l2);
-
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    private static boolean isNumberInRow(int[][] matrix, int number, int row){
+        for (int i = 0; i < 9; i++) {
+            if(matrix[row][i] == number){
+                return true;
+            }
+        }
+        return false;
     }
 
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int sum = l1.val + l2.val + carry;
-        carry  = sum / 10;
-        resList.next = new ListNode(sum % 10);
-        resList = resList.next;
-
-        if(l1.next != null && l2.next != null){
-            addTwoNumbers(l1.next, l2.next);
-        } else if (l1.next != null) {
-            addTwoNumbers(l1.next, new ListNode(0));
-        } else if (l2.next != null) {
-            addTwoNumbers(new ListNode(0), l2.next);
-        }else if(carry > 0){
-            resList.next = new ListNode(1);
-            resList = resList.next;
+    private static boolean isNumberInColumn(int[][] matrix, int number, int column){
+        for (int i = 0; i < 9; i++) {
+            if(matrix[i][column] == number){
+                return true;
+            }
         }
-        return head.next;
+        return false;
+    }
+
+    private static boolean isNumberInBox(int[][] matrix, int number, int row, int column){
+        int currentRow = row - row % 3;
+        int currentColumn = column - column % 3;
+
+        for (int i = currentRow; i < currentRow + 3; i++) {
+            for (int j = currentColumn; j < currentColumn + 3; j++) {
+                if(matrix[i][j] == number){
+                    return true;
+                }
+            }
+        }
+
+        return  false;
+    }
+
+    private static boolean isValidPlacement(int[][] matrix, int number, int row, int column){
+        return !isNumberInRow(matrix,number,row) &&
+                !isNumberInColumn(matrix,number, column) &&
+                !isNumberInBox(matrix, number, row, column);
     }
 }
