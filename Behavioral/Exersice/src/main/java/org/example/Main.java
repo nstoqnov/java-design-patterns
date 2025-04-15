368d964a3c2cc0261c6bb50d5b6f103543b3b02b
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-04-15 13:21:57 +0300

    exercise: phonebook

--- Behavioral/Exersice/src/main/java/org/example/Main.java
+++ Behavioral/Exersice/src/main/java/org/example/Main.java
@@ -5,35 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int[][] matrix = {
-                {5, 3, 4, 6, 7, 8, 9, 1, 2},
-                {6, 0, 2, 0, 9, 5, 3, 4, 8},
-                {1, 9, 8, 3, 4, 2, 5, 6, 7},
-                {8, 5, 9, 7, 6, 1, 4, 2, 3},
-                {4, 2, 6, 8, 5, 3, 7, 9, 1},
-                {7, 1, 3, 9, 2, 4, 8, 5, 6},
-                {9, 6, 1, 5, 3, 7, 2, 8, 4},
-                {2, 0, 7, 0, 1, 9, 6, 3, 5},
-                {3, 4, 5, 2, 8, 6, 1, 7, 9}
-        };
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if(matrix[i][j] == 0){
-                    for (int k = 1; k <= 9; k++) {
-                        if(isValidPlacement(matrix,k,i,j)){
-                            matrix[i][j] = k;
-                        }
-                    }
-                }
-            }
-        }
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
+        int n = Integer.parseInt(scanner.next());
+
+        for (int i = 0; i < n; i++) {
+
         }
     }
     
