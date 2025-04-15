89791a4ebe3a8a6d317e1a4ea7beedfafd899dbb
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-04-15 13:22:09 +0300

    exercise: phonebook

--- Behavioral/Exersice/src/main/java/org/example/Main.java
+++ Behavioral/Exersice/src/main/java/org/example/Main.java
@@ -14,43 +14,5 @@ public class Main {
 
         }
     }
-    
-    private static boolean isNumberInRow(int[][] matrix, int number, int row){
-        for (int i = 0; i < 9; i++) {
-            if(matrix[row][i] == number){
-                return true;
-            }
-        }
-        return false;
-    }
 
-    private static boolean isNumberInColumn(int[][] matrix, int number, int column){
-        for (int i = 0; i < 9; i++) {
-            if(matrix[i][column] == number){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean isNumberInBox(int[][] matrix, int number, int row, int column){
-        int currentRow = row - row % 3;
-        int currentColumn = column - column % 3;
-
-        for (int i = currentRow; i < currentRow + 3; i++) {
-            for (int j = currentColumn; j < currentColumn + 3; j++) {
-                if(matrix[i][j] == number){
-                    return true;
-                }
-            }
-        }
-
-        return  false;
-    }
-
-    private static boolean isValidPlacement(int[][] matrix, int number, int row, int column){
-        return !isNumberInRow(matrix,number,row) &&
-                !isNumberInColumn(matrix,number, column) &&
-                !isNumberInBox(matrix, number, row, column);
-    }
 }
