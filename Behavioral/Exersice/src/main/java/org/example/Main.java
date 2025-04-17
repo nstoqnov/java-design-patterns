0fb8755c9805712ef920a54b3b7146903b342c6e
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-04-17 14:03:51 +0300

    exercise: phonebook

--- Behavioral/Exersice/src/main/java/org/example/Main.java
+++ Behavioral/Exersice/src/main/java/org/example/Main.java
@@ -30,6 +30,13 @@ public class Main {
         }
     }
 
+    private static void delByNumber(String[] phonebook, String phone) {
+        for (int i = 0; i < phonebook.length; i++) {
+            if(phonebook[i] != null && phonebook[i].contains(phone)){
+                phonebook[i] = null;
+            }
+        }
+    }
     private static void getByNumber(String[] phonebook, String phone) {
         for (int i = 0; i < phonebook.length; i++) {
             if(phonebook[i] != null && phonebook[i].contains(phone)){
