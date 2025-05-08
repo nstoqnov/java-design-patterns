6525817c01d28d38d322325d46743ad3ba62b1bf
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-05-08 20:19:30 +0300

    exercise: pron

--- Behavioral/Exersice/src/main/java/org/example/Main.java
+++ Behavioral/Exersice/src/main/java/org/example/Main.java
@@ -5,33 +5,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-        int[] arr = new int[n + 1];
+        int vowels = 0;
+        int consonants = 0;
 
-        for (int i = 0; i < n + 1; i++) {
-            arr[i] = Integer.parseInt(scanner.next());
+        for (int i = 0; i < n; i++) {
+            vowels = 0;
+            consonants = 0;
+            String input = scanner.nextLine();
+
         }
 
-        boolean notSorted = true;
-        int counter = 0;
-        while(notSorted){
-            for (int i = 0; i < arr.length - 1; i++) {
-                notSorted = false;
-
-                if(arr[i] > arr[i + 1]){
-                    int temp = arr[i + 1];
-                    arr[i + 1] = arr[i];
-                    arr[i] = temp;
-                    counter++;
-                    notSorted = true;
-                }
-            }
-        }
-
-        System.out.println(counter*10);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int n = Integer.parseInt(scanner.nextLine());
+//        int[] arr = new int[n + 1];
+//
+//        for (int i = 0; i < n + 1; i++) {
+//            arr[i] = Integer.parseInt(scanner.next());
+//        }
+//
+//        boolean notSorted = true;
+//        int counter = 0;
+//        while(notSorted){
+//            for (int i = 0; i < arr.length - 1; i++) {
+//                notSorted = false;
+//
+//                if(arr[i] > arr[i + 1]){
+//                    int temp = arr[i + 1];
+//                    arr[i + 1] = arr[i];
+//                    arr[i] = temp;
+//                    counter++;
+//                    notSorted = true;
+//                }
+//            }
+//        }
+//
+//        System.out.println(counter*10);
     }
 }
