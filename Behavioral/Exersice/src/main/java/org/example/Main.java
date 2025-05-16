51affe8e5970e6bce2b766b89b295f386a1363d8
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-05-16 16:43:14 +0300

    exercise: digit multiplication

--- Behavioral/Exersice/src/main/java/org/example/Main.java
+++ Behavioral/Exersice/src/main/java/org/example/Main.java
@@ -7,20 +7,14 @@ public class Main {
     public static void main(String[] args) {
         //Digit multiplication
         Scanner scanner = new Scanner(System.in);
-
         int n = Integer.parseInt(scanner.next());
-        int result = 1;
-        while (n > 9){
-            result = (n % 10) * result;
-            n = n/10;
-        }
-        n = result;
-        result = 1;
-        while(n > 9){
-            result = result * n % 10;
-            n = n/10;
-        }
-
     }
-
+    public static void divideToOneDigit(int a){
+        int result = 1;
+        while (a > 9){
+            result = (a % 10) * result;
+            a = a / 10;
+        }
+        a = result;
+    }
 }
