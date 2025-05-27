e69496d677dc1ea8bafa793cf1dac8990264962a
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-05-27 17:14:05 +0300

    myregex exercice

--- Behavioral/Exersice/src/main/java/org/example/Main.java
+++ Behavioral/Exersice/src/main/java/org/example/Main.java
@@ -6,23 +6,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //Digit multiplication
-        Scanner scanner = new Scanner(System.in);
-        Long n = Long.parseLong(scanner.next());
-        while (n > 9){
-            n = divideToOneDigit(n);
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(MyRegex.pattern));
         }
-        System.out.println(n);
+        in.close();
     }
-    public static Long divideToOneDigit(Long a){
-        Long result = a % 10;
-        if(result == 0){
-            return 0L;
-        }
-        a = a / 10;
-        while (a % 10 > 0){
-            result = (a % 10) * result;
-            a = a / 10;
-        }
-        return result;
-    }
+
 }
---
+++ Behavioral/Exersice/src/main/java/org/example/MyRegex.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class MyRegex {
+    public static final String pattern =
+            "^((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)\\.){3}"
+                    + "(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)$";
+}
