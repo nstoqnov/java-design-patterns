e1d6c1a44e65e17c3ed14c508aa056fc1574299c
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-05-30 17:18:50 +0300

    myregex exercice

--- Behavioral/Exersice/src/main/java/org/example/Main.java
+++ Behavioral/Exersice/src/main/java/org/example/Main.java
@@ -2,15 +2,27 @@ package org.example;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        //Digit multiplication
+        String regex = "\\b(\\w+)(\\s+\\1\\b)+";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
         Scanner in = new Scanner(System.in);
-        while(in.hasNext()){
-            String IP = in.next();
-            System.out.println(IP.matches(MyRegex.pattern));
+
+        while (in.hasNextLine()) {
+            String input = in.nextLine();
+            Matcher m = p.matcher(input);
+
+            while (m.find()) {
+                input = input.replaceAll(m.group(), m.group(1));
+            }
+
+            System.out.println(input);
         }
+
         in.close();
     }
 
