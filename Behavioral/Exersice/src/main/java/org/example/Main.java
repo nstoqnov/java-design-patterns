465ce6f67cf745c33eba3733a552f43d2ffdb7f7
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-06-02 17:21:15 +0300

    tag content exercise

--- Behavioral/Exersice/src/main/java/org/example/Main.java
+++ Behavioral/Exersice/src/main/java/org/example/Main.java
@@ -7,23 +7,10 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String regex = "\\b(\\w+)(\\s+\\1\\b)+";
-        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-
         Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
 
-        while (in.hasNextLine()) {
-            String input = in.nextLine();
-            Matcher m = p.matcher(input);
-
-            while (m.find()) {
-                input = input.replaceAll(m.group(), m.group(1));
-            }
-
-            System.out.println(input);
-        }
-
-        in.close();
+        Pattern pattern = Pattern.compile("<(.+?)>([^<>]+)</\\1>");
     }
 
 }
