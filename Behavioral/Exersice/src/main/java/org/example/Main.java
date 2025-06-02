2d823afb19d3fe82042ff3278890bdd3bee51ca5
Nikolay Stoyanov <stoqnovn@yahoo.com>
2025-06-02 17:22:08 +0300

    tag content exercise

--- Behavioral/Exersice/src/main/java/org/example/Main.java
+++ Behavioral/Exersice/src/main/java/org/example/Main.java
@@ -21,6 +21,9 @@ public class Main {
                 System.out.println(matcher.group(2)); // print the content
                 found = true;
             }
+            if (!found) {
+                System.out.println("None");
+            }
         }
     }
 
